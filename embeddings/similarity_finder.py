c2ff604579dbbd74418c604681dd6a420fd61277
Bianca Ciocan <ciocanm215@gmail.com>
2026-06-28 12:54:12 +0300

    Similarity Finder

--- embeddings/similarity_finder.py
+++ embeddings/similarity_finder.py
@@ -4,7 +4,7 @@ import os
 
 from langchain_community.embeddings import OllamaEmbeddings
 import numpy as np
-embeddings = OllamaEmbeddings(model = "nomic-embed-text")
+embeddings = OllamaEmbeddings(model = "llama3.2")
 
 text1 = input("enter the text_1 ")
 text2 = input("enter the text_2")
@@ -14,4 +14,4 @@ response2 = embeddings.embed_query(text2)
 
 similarity_score = np.dot(response1, response2) / (np.linalg.norm(response1) * np.linalg.norm(response2))
 
-print(similarity_score)
+print(similarity_score*100, "%")
