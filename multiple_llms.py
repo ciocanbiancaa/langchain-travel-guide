812effb859aa64d3aeedea7b63cf0ee6beb88c22
Bianca Ciocan <ciocanm215@gmail.com>
2026-06-28 11:23:38 +0300

    Multiple llms

---
+++ multiple_llms.py
@@ -0,0 +1,42 @@
+import os
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+llm1 = Ollama(model = "llama3.2")
+llm2 = Ollama(model = "mistral")
+title_prompt = PromptTemplate(
+    input_variables = ["topic"],
+    template= """You are an experienced speech writer.
+    You need to craft an impactful title for a speech 
+    on the following topic: {topic}
+    Answer exactly with one title."""
+
+)
+
+speech_prompt = PromptTemplate(
+    input_variables = ["title"],
+    template= """You need to write a powerful speech of 350 words
+    for the following title: {title}"""
+
+
+)
+
+
+chain_1 = title_prompt | llm1 | StrOutputParser() | (lambda title: (st.write(title),title)[1])
+chain_2 = speech_prompt | llm2
+final_chain = chain_1 | chain_2
+
+st.title("Speech Generator")
+
+topic = st.text_input("Enter the topic: ")
+
+
+if topic:
+    response = final_chain.invoke({"topic": topic})
+
+    st.write(response)
+
